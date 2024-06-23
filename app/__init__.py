afece8f158d8f84ef1d6bbd0a4de8bc468fffeee
YaBoiAli <alinawab9830@gmail.com>
2024-06-23 00:48:51 -0500

    changed hobbies a little bit and added experiences

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,6 @@ def hobbies():
     hobbies = {
         "Creative Pursuits": [
             {"name": "Calligraphy", "description": "Practicing beautiful handwriting and letter design.", "image": "./static/img/57810812c56f8f67b043e6c95a802408.jpg"},
-            {"name": "Poetry", "description": "Crafting verses and exploring literary expressions.", "image": "./static/img/images.jpeg"},
             {"name": "Painting", "description": "Experimenting with colors and techniques on canvas.", "image": "./static/img/painting.jpeg"}
         ],
         "Active and Culinary": [
@@ -47,9 +46,47 @@ def hobbies():
     return render_template('hobbies.html', hobbies=hobbies, menu=active_menu(nav_menu, '/hobbies'))
 
 
+experience_data = [
+    {
+        'title': 'MLH Fellow - SWE/SRE DevOps Intern',
+        'company': 'MLH Fellowship',
+        'location': 'Remote',
+        'dates': 'June 2024 - Present',
+        'description': 'MLH Fellow working as a SWE/SRE DevOps Intern at Meta.',
+    },
+    {
+        'title': 'Software Engineer',
+        'company': 'AI Healthcare Startup',
+        'location': 'Remote',
+        'dates': 'May 2024 - Present',
+        'description': 'Partnered with Google and Amazon Developers on healthcare solution startup. Utilized React & Tailwind components to initialize the webpage using 500+ crucial lines of code. Next.js, Tailwind, git, GitHub, Cloud dev, Web development, markdown, Firebase.',
+    },
+    {
+        'title': 'Artificial Intelligence Engineer Intern',
+        'company': 'Radical AI',
+        'location': 'Remote',
+        'dates': 'February 2024 - May 2024',
+        'description': 'Proactively engaged in code reviews, debugging, and troubleshooting, resulting in a 10% reduction in system errors. Demonstrated proficiency in both front-end and back-end development, utilizing JS, Tailwind, and PostgreSQL. Developed ReX, an AI Coach utilizing technologies like OpenAI, Vertex AI, and TensorFlow.',
+    },
+    {
+        'title': 'Software Engineer Intern',
+        'company': 'Woojoo Universe',
+        'location': 'Remote',
+        'dates': 'December 2023 - April 2024',
+        'description': 'Significantly contributed to cross-platform mobile app development (React.js, React Native, Java) with a notable 30% increase in user engagement. Implemented GraphQL for 25% faster data querying and enhanced SQL database efficiency by 10%. Conducted API testing with Insomnia, ensuring a remarkable 95% error-free interface and streamlining build processes.',
+    },
+    {
+        'title': 'Software Engineer',
+        'company': 'Upwork',
+        'location': 'Irving, Texas',
+        'dates': 'August 2022 - December 2023',
+        'description': 'Designed, debugged, and built software applications, maintaining high code quality and performance standards. Collaborated with engineers to architect, implement, and test new features, contributing to the platform\'s evolution. Engaged in agile development practices, ensuring regular software releases and alignment with business objectives.',
+    },
+]
+
 @app.route('/experience')
 def experience():
-    return render_template('experience.html', title="Experience", url=os.getenv("URL"), menu=active_menu(nav_menu, '/experience'))
+    return render_template('experience.html', title="Experience", url=os.getenv("URL"), menu=active_menu(nav_menu, '/experience'), experience=experience_data)
 
 
 @app.route('/education')
