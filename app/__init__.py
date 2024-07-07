788d9b430ce7f21c9002f382bb802958b131368a
YaBoiAli <alinawab9830@gmail.com>
2024-07-08 00:28:11 +0300

    new port and new app run

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 # Links declared for dynamic rendering in template
 nav_menu = [
