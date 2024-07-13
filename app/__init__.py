94de397673bb600111159e332d2a1aa8439fd1cc
YaBoiAli <alinawab9830@gmail.com>
2024-07-13 23:05:19 +0300

    a lil scret

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 # Links declared for dynamic rendering in template
